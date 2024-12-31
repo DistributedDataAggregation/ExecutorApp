796731f7f91414a261d96361341ab93898ebf364
Kapiszon <krol.kacper.pg2@gmail.com>
2024-12-31 02:28:49 +0100

    feat: update interface

--- src/hash_table/hash_table_abstraction.h
+++ src/hash_table/hash_table_abstraction.h
@@ -1,12 +1,21 @@
 #ifndef HASH_TABLE_ABSTRACTION_H
 #define HASH_TABLE_ABSTRACTION_H
+#include "hash_table_struct.h"
+#include "error_handling.h"
+#include "query_response.pb-c.h"
 
-typedef struct {
-    void (*insert)(void* table, const char* key, void* value);
-    void* (*search)(void* table, const char* key);
-    void (*remove)(void* table, const char* key);
-    void (*print)(void* table);
-    void (*free)(void* table);
-} HashTableAbstraction;
+typedef struct HashTableInterface {
+    unsigned int (*hash)(const char* string, int table_size);
+    HashTable* (*create)(int size, ErrorInfo* err);
+    void (*free)(HashTable* table);
+    void (*insert)(HashTable* table, HashTableEntry* entry, ErrorInfo* err);
+    HashTableEntry* (*search)(const HashTable* table, const char* key);
+    void (*remove)(HashTable* table, const char* key);
+    void (*print)(const HashTable* ht);
+    void (*combine_entries)(HashTableEntry* entry1, const HashTableEntry* entry2, ErrorInfo* err);
+    HashTableValue (*update_value)(HashTableValue current_value, HashTableValue incoming_value, ErrorInfo* err);
+    void (*combine_with_response)(HashTable* ht, const QueryResponse* query_response, ErrorInfo* err);
+    void (*optimized_combine)(HashTable* destination, const HashTable* source, ErrorInfo* err);
+} HashTableInterface;
 
 #endif //HASH_TABLE_ABSTRACTION_H
